0bbaf10bb6c1b4fbd2bb106f87ab89f58f788af9
Mitchell <mastrafton@gmail.com>
2021-01-25 22:28:56 -0600

    added visual studio junk

---
+++ Assignment1/Wocconsts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Woc_consts 
+{
+    public enum itemType
+    {
+    Helmet, Neck, Shoulders, Back, Chest, Wrist,
+    Gloves, Belt, Pants, Boots, Ring, Trinket, None
+    };
+    public enum Race { Orc, Troll, Tauren, Forsaken};
+}
