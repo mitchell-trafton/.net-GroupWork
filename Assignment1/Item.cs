077a8000dc34bff52e314d9236793993c01f0853
rttrotter <58933487+rttrotter@users.noreply.github.com>
2021-01-26 00:12:11 -0600

    Add files via upload

--- Assignment1/Item.cs
+++ Assignment1/Item.cs
@@ -5,20 +5,78 @@ namespace Woc_item
     class Item : IComparable
     {
         //constants
-        private readonly static uint MAX_ILVL = 360;
-        private readonly static uint MAX_PRIMARY = 200;
-        private readonly static uint MAX_LEVEL = 60;
-        private readonly static uint GEAR_SLOTS = 14;
-        private readonly static uint MAX_INVENTORY_SIZE = 20;
-        // variables
-        public readonly int id; //identifer for the item 
-        public string name; // item name
-        public itemType type; //where the item can be placed
-        public uint ilvl;//
-        public uint primary;
-        public uint stamina;
-        public uint requirement;
-        public string flavor; // flavor text
+        // private attributes
+        private int id; //identifer for the item 
+        private string name; // item name
+        private itemType type; //where the item can be placed
+        private uint ilvl;//
+        private uint primary;
+        private uint stamina;
+        private uint requirement;
+        private string flavor; // flavor text
+        // public properties
+        public int Id
+        {
+            get { return id; }
+
+            private set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public itemType Type
+        {
+            get { return type; }
+            set
+            {
+                if ((int)value < 0 || (int)value > 12) throw new ArgumentOutOfRangeException($"{nameof(value)} must be between 0 and 12."); // chck to see if data is within bounds otherwise throw exception
+                type = value;
+            }
+        }
+        public uint Ilvl
+        {
+            get { return ilvl; }
+            set
+            {
+                if (value > Constants.MAX_ILVL) throw new ArgumentOutOfRangeException($"{nameof(value)} must be between 0 and {Constants.MAX_ILVL}.");
+                ilvl = value;
+            }
+        }
+        public uint Primary
+        {
+            get { return primary; }
+            set
+            {
+                if (value > Constants.MAX_PRIMARY) throw new ArgumentOutOfRangeException($"{nameof(value)} must be between 0 and {Constants.MAX_PRIMARY}.");
+                primary = value;
+            }
+        }
+        public uint Stamina
+        {
+            get { return stamina; }
+            set
+            {
+                if (value > Constants.MAX_STAMINA) throw new ArgumentOutOfRangeException($"{nameof(value)} must be between 0 and {Constants.MAX_STAMINA}.");
+                stamina = value;
+            }
+        }
+        public uint Requirement
+        {
+            get { return requirement; }
+            set
+            {
+                if (value > Constants.MAX_LEVEL) throw new ArgumentOutOfRangeException($"{nameof(value)} must be between 0 and {Constants.MAX_LEVEL}.");
+                requirement = value;
+            }
+        }
+
+        public string Flavor
+        {
+            get { return flavor; }
+            set { flavor = value; }
+        }
         public Item()
         {
             id = 0;
