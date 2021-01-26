71e67c6aee8daf5e6dd10b963a1407226624b44a
Mitchell <mastrafton@gmail.com>
2021-01-25 23:19:18 -0600

    moved constants to woc_consts namespace

--- Assignment1/Wocconsts.cs
+++ Assignment1/Wocconsts.cs
@@ -7,4 +7,14 @@ namespace Woc_consts
     Gloves, Belt, Pants, Boots, Ring, Trinket, None
     };
     public enum Race { Orc, Troll, Tauren, Forsaken};
+
+    static class Constants
+    {
+        public static uint MAX_ILVL = 360;
+        public static uint MAX_PRIMARY = 200;
+        public static uint MAX_STAMINA = 275;
+        public static uint MAX_LEVEL = 60;
+        public static uint GEAR_SLOTS = 14;
+        public static uint MAX_INVENTORY_SIZE = 20;
+    }
 }
