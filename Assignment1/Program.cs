984d1841a5df10bd658e3795b32f39a40aae4548
mitchell-trafton <59815180+mitchell-trafton@users.noreply.github.com>
2021-01-27 01:32:33 -0600

    Update Program.cs

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -6,23 +6,7 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Player player1 = new Player(Name:"a");
-
-            player1.Exp = 3000;
-
-            Console.WriteLine(player1.Level);
-
-            player1.Exp = 2000;
-
-            Console.WriteLine(player1.Level);
-
-            player1.Exp = 2000;
-
-            Console.WriteLine(player1.Level);
-
-            player1.Exp = 4000;
-
-            Console.WriteLine(player1.Level);
+            
         }
     }
 }
